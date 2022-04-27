e98cc470a6cf2a1bc7b328ce4293c2476ccab6bf
Tomás Migone <tomasmigone@gmail.com>
2022-04-27 17:08:23 -0300

    Add solidity linting and formating
    Signed-off-by: Tomás Migone <tomasmigone@gmail.com>

--- contracts/Trust.sol
+++ contracts/Trust.sol
@@ -2,45 +2,70 @@
 pragma solidity ^0.8.0;
 
 contract Trust {
-  address public beneficiary;
-  uint public unlockDate;
+    address public beneficiary;
+    uint256 public unlockDate;
 
-  event Deposited(address indexed from, address indexed beneficiary, uint amount, uint balance);
-  event Withdrawn(address indexed beneficiary, uint balance);
-  event NewBeneficiary(address indexed from, address indexed to);
+    event Deposited(
+        address indexed from,
+        address indexed beneficiary,
+        uint256 amount,
+        uint256 balance
+    );
+    event Withdrawn(address indexed beneficiary, uint256 balance);
+    event NewBeneficiary(address indexed from, address indexed to);
 
-  modifier onlyBeneficiary {
-    require(msg.sender == beneficiary, "Sender is not the beneficiary.");
-    _;
-  }
+    modifier onlyBeneficiary() {
+        require(msg.sender == beneficiary, "Sender is not the beneficiary.");
+        _;
+    }
 
-  constructor(address _beneficiary, uint _unlockDate) payable {
-    require(_beneficiary != address(0), "Beneficiary cannot be the zero address.");
-    require(_unlockDate > block.timestamp, "Unlock date must be in the future.");
-    require(msg.value == 0, "Initial deposit must be zero.");
+    constructor(address _beneficiary, uint256 _unlockDate) payable {
+        require(
+            _beneficiary != address(0),
+            "Beneficiary cannot be the zero address."
+        );
+        require(
+            _unlockDate > block.timestamp,
+            "Unlock date must be in the future."
+        );
+        require(msg.value == 0, "Initial deposit must be zero.");
 
-    beneficiary = _beneficiary;
-    unlockDate = _unlockDate;
-  }
+        beneficiary = _beneficiary;
+        unlockDate = _unlockDate;
+    }
 
-  receive() external payable {
-    require(unlockDate > block.timestamp, "Wallet is unlocked, can't deposit.");
-    require(msg.value > 0, "Deposit must be greater than zero.");
-    
-    emit Deposited(msg.sender, beneficiary, msg.value, address(this).balance);
-  }
+    receive() external payable {
+        require(
+            unlockDate > block.timestamp,
+            "Wallet is unlocked, can't deposit."
+        );
+        require(msg.value > 0, "Deposit must be greater than zero.");
 
-  function withdraw() external onlyBeneficiary {
-    require(block.timestamp >= unlockDate, "Wallet is locked.");
+        emit Deposited(
+            msg.sender,
+            beneficiary,
+            msg.value,
+            address(this).balance
+        );
+    }
 
-    emit Withdrawn(beneficiary, address(this).balance);
-    payable(beneficiary).transfer(address(this).balance);
-  }
+    function withdraw() external onlyBeneficiary {
+        require(block.timestamp >= unlockDate, "Wallet is locked.");
 
-  function transferBeneficiary(address _beneficiary) external onlyBeneficiary {
-    require(_beneficiary != address(0), "Beneficiary cannot be the zero address.");
+        emit Withdrawn(beneficiary, address(this).balance);
+        payable(beneficiary).transfer(address(this).balance);
+    }
 
-    beneficiary = _beneficiary;
-    emit NewBeneficiary(msg.sender, beneficiary);
-  }
+    function transferBeneficiary(address _beneficiary)
+        external
+        onlyBeneficiary
+    {
+        require(
+            _beneficiary != address(0),
+            "Beneficiary cannot be the zero address."
+        );
+
+        beneficiary = _beneficiary;
+        emit NewBeneficiary(msg.sender, beneficiary);
+    }
 }
