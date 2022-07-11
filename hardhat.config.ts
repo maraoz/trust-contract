6089cbaa4590c40309e0100687781f2f8d84415d
Manuel Araoz <287189+maraoz@users.noreply.github.com>
2022-07-11 14:37:44 -0300

    Add optimism network config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,6 +5,14 @@ import { HardhatUserConfig } from "hardhat/config";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.4",
+  networks: {
+    optimism: {
+      url: `https://optimism-mainnet.infura.io/v3/${process.env.INFURA_ID}`,
+      accounts: process.env.MNEMONIC ? {
+        mnemonic: process.env.MNEMONIC,
+      } : undefined,
+    }
+  }
 };
 
 export default config;
